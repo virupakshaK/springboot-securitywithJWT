9902cec7802582037895f00124c1b08607c9b6b3
virupakshaK <virupaksha579@gmail.com>
2021-07-06 00:50:55 +0530

    Base code skelton commit

---
+++ src/main/java/com/veeru/springboot/security/start/SpringbootSecuritywithJwtApplication.java
@@ -0,0 +1,13 @@
+package com.veeru.springboot.security.start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSecuritywithJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSecuritywithJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/veeru/springboot/security/start/SpringbootSecuritywithJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veeru.springboot.security.start;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSecuritywithJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
